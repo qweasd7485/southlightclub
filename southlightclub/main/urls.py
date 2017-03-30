399e54cfca1b7d44034b8fca11c5608f513b222c
Lester Shih <u0124093@gmail.com>
2017-03-30 12:32:52 +0800

    third push

--- southlightclub/main/urls.py
+++ southlightclub/main/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 from main import views
 
-urlpatterns = [
-    url(r'^$', views.main, name='main'),
+urlpatterns = [    
     url(r'^about/$', views.about, name='about'),
     url(r'^president/$', views.president, name='president'),
     url(r'^words/$', views.words, name='words'),
     url(r'^target/$', views.target, name='target'),    
+    url(r'^$', views.main, name='main'),
 ]
