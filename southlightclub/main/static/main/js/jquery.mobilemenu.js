a96313daa1dd0e47da0275539c450cebce6013ca
Lester Shih <u0124093@gmail.com>
2017-04-06 10:35:33 +0800

    1.修改style.css中不以https運行會出錯的錯誤 2.調整FB粉絲專頁連結rwd 3.調整項目導覽行上下寬度 4.調整FB粉絲專頁連結大小

--- southlightclub/main/static/main/js/jquery.mobilemenu.js
+++ southlightclub/main/static/main/js/jquery.mobilemenu.js
@@ -13,7 +13,7 @@
 $.fn.mobileMenu = function(options) {
   
   var defaults = {
-      defaultText: 'Navigate to...',
+      defaultText: '首頁',
       className: 'select-menu',
       subMenuClass: 'sub-menu',
       subMenuDash: '&ndash;'
